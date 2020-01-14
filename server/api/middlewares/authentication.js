32aa5691b9b92256d27a9e40bba0451c97e7b371
mjavad <mjavad.alj@yahoo.com>
2020-01-14 13:04:58 +0330

    login authentication middleware fixed

--- server/api/middlewares/authentication.js
+++ server/api/middlewares/authentication.js
@@ -3,4 +3,9 @@ const passport = require('passport');
 
 module.exports.authenticated = (req, res, next) => {
     if (req.isAuthenticated()) return next();
+    else {
+        return res.status(400).json({
+            message: "you are not allowed - please login"
+        })
+    }
 }
