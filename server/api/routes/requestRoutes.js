319e3a2ca1c70c14ba152cdb8485069f76368842
mjavad <mjavad.alj@yahoo.com>
2020-01-17 15:55:30 +0330

    development environment fixed

--- server/api/routes/requestRoutes.js
+++ server/api/routes/requestRoutes.js
@@ -1,20 +1,20 @@
 const router = require('express').Router();
 const requestController = require('../controllers/requestController');
-const authetication = require('../middlewares/authentication');
+const authentication = require('../middlewares/authentication');
 const logger = require('../middlewares/logger');
 
-if (process.env.NODE_ENV == "production ") {
-    router.post('/addRequest', authetication.authenticated, logger.requestLogger, requestController.addRequest);
+if (process.env.NODE_ENV == 'production ') {
+    router.post('/addRequest', authentication.authenticated, logger.requestLogger, requestController.addRequest);
 
-    router.get('/getUserRequests', authetication.authenticated, logger.requestLogger, requestController.getUserRequests);
-    router.post('/getRequest', authetication.authenticated, logger.requestLogger, requestController.getRequest);
+    router.get('/getUserRequests', authentication.authenticated, logger.requestLogger, requestController.getUserRequests);
+    router.post('/getRequest', authentication.authenticated, logger.requestLogger, requestController.getRequest);
 
-} else if (process.env.NODE_ENV == "development " || process.env.NODE_ENV == "test ") {
+} else if (process.env.NODE_ENV == 'development ' || process.env.NODE_ENV == 'test ') {
 
-    router.post('/addRequest', logger.requestLogger, requestController.addRequest);
+    router.post('/addRequest', logger.requestLogger, requestController.devAddRequest);
 
-    router.post('/getUserRequests', logger.requestLogger, requestController.getUserRequests);
-    router.post('/getRequest', logger.requestLogger, requestController.getRequest);
+    router.post('/getUserRequests', logger.requestLogger, requestController.devGetUserRequests);
+    router.post('/getRequest', logger.requestLogger, requestController.devGetRequest);
 
 }
 
